a8efab3b04be7ddbdea644d9143f865b6a761d86
Claudia Pellegrino <claui@users.noreply.github.com>
2022-10-02 14:42:55 +0200

    Print usage hint if no args given

--- release_feed_mediola/cli.py
+++ release_feed_mediola/cli.py
@@ -1,11 +1,16 @@
 """Entry point for the command line interface."""
 
 import sys
+from typing import NoReturn
 
 from . import api
 
 
-def run(*args: str) -> None:
+def run(*args: str) -> NoReturn:
     """Runs the command line interface."""
-    combined_args = list(args) + sys.argv[1:]
+    if not (combined_args := list(args) + sys.argv[1:]):
+        print(f'Usage: {sys.argv[0]} [product_name]',
+              file=sys.stderr)
+        sys.exit(1)
     print(api.release_feed(*combined_args[:1]))
+    sys.exit(0)
