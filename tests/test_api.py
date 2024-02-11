954e9c35152fbbcaf95896bf7a16407a939f248d
Claudia Pellegrino <claui@users.noreply.github.com>
2024-02-11 12:36:03 +0100

    Bump feedgen to v1.0

--- tests/test_api.py
+++ tests/test_api.py
@@ -45,14 +45,14 @@ def test_from_dict(neo_package: dict[str, Any],
   <title>neo – Releases</title>
   <updated>2022-10-01T14:46:53+02:00</updated>
   <link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo" rel="alternate" type="text/html"/>
-  <generator uri="https://lkiesow.github.io/python-feedgen" version="0.9.0">python-feedgen</generator>
+  <generator uri="https://lkiesow.github.io/python-feedgen" version="1.0.0">python-feedgen</generator>
   <subtitle>Software releases for neo</subtitle>
   <entry>
     <id>https://mediola.com/service/downloads/versions/2.11.3</id>
     <title>2.11.3</title>
     <updated>2022-09-26T00:00:00+02:00</updated>
     <content>Version 2.11.3</content>
-    <link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo" rel="alternate" type="text/html"/>
+    <link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo"/>
     <published>2022-09-26T00:00:00+02:00</published>
     <rights>https://www.mediola.com/eula</rights>
   </entry>
