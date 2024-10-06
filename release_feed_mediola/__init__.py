3772ffb8a74e066e4bed09599b74a7687caf839e
Claudia Pellegrino <claui@users.noreply.github.com>
2024-10-06 18:59:10 +0200

    Fix regression

--- release_feed_mediola/__init__.py
+++ release_feed_mediola/__init__.py
@@ -6,13 +6,13 @@
 
 # Re-export these symbols
 # (This promotes them from release_feed_mediola.api to release_feed_mediola)
-from release_feed_mediola.api import Api as Api
+from release_feed_mediola.api import release_feed as release_feed
 
 from release_feed_mediola.version import version
 
 __all__ = [
     # Tell pdoc to pick up all re-exported symbols
-    "Api",
+    "release_feed",
     # Modules that every subpackage should see
     # (This also exposes them to pdoc)
     "api",
