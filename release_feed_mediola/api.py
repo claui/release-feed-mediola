1cea753f57e0c86461318270a06ef1480de7eef2
Claudia Pellegrino <claui@users.noreply.github.com>
2024-10-06 18:57:11 +0200

    redport: merge upstream changes
    Upstream commit: d9f839e34864cbd0c38f9bfd53e9d81aa1c2a2d1
    
    Redport-State: merged-rebased
    
    # Conflicts:
    #	pyproject.toml
    #	release_feed_mediola/api.py
    #	release_feed_mediola/cli.py

--- release_feed_mediola/api.py
+++ release_feed_mediola/api.py
@@ -19,10 +19,10 @@ from .settings import (
 logger = get_logger(__name__)
 
 
-class Api:  # pylint:disable=too-few-public-methods
+def release_feed(product_name: str | None) -> str:
     """Generates a release feed for the given package name.
 
-    :param `product_name`:
+    :param product_name:
         the product for which to generate a feed.
         Must be one of the values `aioremote`, `aioremote_desktop`,
         `configtool`, `configtoolneo`, `firmware`, `iqontrol`,
@@ -31,38 +31,22 @@ class Api:  # pylint:disable=too-few-public-methods
 
     :raises ValueError:
         if `product_name` is not one of the valid names.
+
+    :return: a release feed for `product_name`.
     """
 
-    def release_feed(
-        self, product_name: str | None
-    ) -> str:  # pylint: disable=no-self-use
-        """Generates a release feed for the given package name.
-
-        :param `product_name`:
-            the product for which to generate a feed.
-            Must be one of the values `aioremote`, `aioremote_desktop`,
-            `configtool`, `configtoolneo`, `firmware`, `iqontrol`,
-            `iqontrol_neo`, `neo`, `neoserver`, `neoserver_ccu3`,
-            `qrcompanion` and `steckerpro`.
-
-        :raises ValueError:
-            if `product_name` is not one of the valid names.
-
-        :return: a release feed for `product_name`.
-        """
-
-        releases_by_version = _download_releases_by_version()
-        packages_by_name = releases_by_version[FEED_SOURCE_LANGUAGE]["software"]
-        if not product_name:
-            raise ValueError("Name cannot be empty.")
-        if product_name not in packages_by_name:
-            raise ValueError(
-                f'Name must be one of: {", ".join(MEDIOLA_PRODUCTS)}',
-            )
-        return feed.from_dict(
-            product_name,
-            packages_by_name[product_name],
+    releases_by_version = _download_releases_by_version()
+    packages_by_name = releases_by_version[FEED_SOURCE_LANGUAGE]["software"]
+    if not product_name:
+        raise ValueError("Name cannot be empty.")
+    if product_name not in packages_by_name:
+        raise ValueError(
+            f'Name must be one of: {", ".join(MEDIOLA_PRODUCTS)}',
         )
+    return feed.from_dict(
+        product_name,
+        packages_by_name[product_name],
+    )
 
 
 def _download_releases_by_version() -> dict[str, Any]:
--- release_feed_mediola/cli.py
+++ release_feed_mediola/cli.py
@@ -1,5 +1,7 @@
 """Entry point for the command line interface."""
 
+from collections.abc import Generator
+from contextlib import contextmanager
 import os
 import sys
 from typing import NoReturn
@@ -9,10 +11,32 @@ import fire  # type: ignore
 from . import __version__, api, fire_workarounds
 from .errors import CliError
 from .logging import get_logger
-from .settings import MEDIOLA_PRODUCTS, PROJECT_ROOT, PYPROJECT_TOML
+from .settings import PROJECT_ROOT, PYPROJECT_TOML
 
 
 logger = get_logger(__name__)
+
+
+def run(*args: str) -> None:
+    """Runs the command line interface."""
+    with _cli_context(*args) as combined_args:
+        fire.Fire(api.release_feed, command=combined_args)
+
+
+@contextmanager
+def _cli_context(*args: str) -> Generator[list[str], None, NoReturn]:
+    combined_args = list(args) + sys.argv[1:]
+    if combined_args and combined_args[0] in {"-V", "--version"}:
+        print(_version_text())
+        sys.exit(0)
+
+    fire_workarounds.apply()
+    try:
+        yield combined_args
+    except CliError as e:
+        logger.error(e)
+        sys.exit(1)
+    sys.exit(0)
 
 
 def _version_text() -> str:
@@ -24,27 +48,3 @@ def _version_text() -> str:
             + f" (in development at {PROJECT_ROOT})"
         )
     return f"release-feed-mediola v{__version__}"
-
-
-def run(*args: str) -> NoReturn:
-    # pylint: disable=magic-value-comparison
-    """Runs the command line interface."""
-
-    if not (combined_args := list(args) + sys.argv[1:]):
-        print(
-            f'Usage: {sys.argv[0]} [{" | ".join(MEDIOLA_PRODUCTS)}]',
-            file=sys.stderr,
-        )
-        sys.exit(1)
-
-    if sys.argv[1:] and sys.argv[1:][0] in {"-V", "--version"}:
-        print(_version_text())
-        sys.exit(0)
-
-    fire_workarounds.apply()
-    try:
-        fire.Fire(api.Api, command=combined_args)
-    except CliError as e:
-        logger.error(e)
-        sys.exit(1)
-    sys.exit(0)
