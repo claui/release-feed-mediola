86ab0e53dec27b7a5215b769ba3dc31df0d58de2
Claudia Pellegrino <claui@users.noreply.github.com>
2024-10-06 20:44:53 +0200

    Migrate to feedgenerator
    Even though feedgen is actively maintained, feedgenerator is available
    as a package on Debian, so use that instead.

--- release_feed_mediola/feed.py
+++ release_feed_mediola/feed.py
@@ -4,7 +4,7 @@ from collections.abc import Callable
 from datetime import date, datetime, time
 from typing import Any
 
-from feedgen.feed import FeedGenerator  # type: ignore
+import feedgenerator  # type: ignore
 
 
 from .settings import (
@@ -22,7 +22,7 @@ INFO = "info"
 def from_dict(
     product_name: str,
     releases_by_version: dict[str, Any],
-    now: Callable[..., datetime] = lambda: datetime.now(MEDIOLA_IMPLIED_TIMEZONE),
+    _now: Callable[..., datetime] = lambda: datetime.now(MEDIOLA_IMPLIED_TIMEZONE),
 ) -> str:
     """Generates an Atom feed from a given releases-by-version
     dict.
@@ -36,6 +36,9 @@ def from_dict(
         an optional supplier of the current system time in form of
         a `datetime`. If a supplier is given, it must return a
         `datetime` whose timezone is defined.
+        Note: the current implementation no longer uses the current
+        system time. The parameter is left for backwards
+        compatibility.
 
     :return: the generated Atom feed as a string.
     """
@@ -43,28 +46,24 @@ def from_dict(
         release[INFO] for _, release in releases_by_version.items() if INFO in release
     )
     context = {"product_name": product_name}
-    web_link = {
-        "href": DOWNLOADS_WEB_URL_TEMPLATE.format(**context),
-        "rel": "alternate",
-        "type": "text/html",
-    }
-    generator = FeedGenerator()
-    generator.id(FEED_NAMESPACE)
-    generator.title(FEED_TITLE_TEMPLATE.format(**context))
-    generator.language(FEED_LANGUAGE)
-    generator.link(**web_link)
-    generator.description(FEED_DESCRIPTION_TEMPLATE.format(**context))
+    feed = feedgenerator.Atom1Feed(
+        id=FEED_NAMESPACE,
+        title=FEED_TITLE_TEMPLATE.format(**context),
+        link=DOWNLOADS_WEB_URL_TEMPLATE.format(**context),
+        description=FEED_DESCRIPTION_TEMPLATE.format(**context),
+        language=FEED_LANGUAGE,
+    )
     for info in filtered_release_infos:
-        entry = generator.add_entry()
-        entry.id(f"{FEED_NAMESPACE}/versions/{info["version"]}")
-        entry.title(info["version"])
-        entry.description(f"Version {info["version"]}")
-        entry.link(**web_link)
-        entry.rights(info["license"])
-        entry.pubDate(_datetime_from_iso_date(info.get("releasedate")))
-        entry.updated(_datetime_from_iso_date(info.get("releasedate")))
-    generator.lastBuildDate(now())
-    return str(generator.atom_str(pretty=True), encoding="utf-8")
+        feed.add_item(
+            unique_id=f"{FEED_NAMESPACE}/versions/{info["version"]}",
+            title=info["version"],
+            description=f"Version {info["version"]}",
+            link=DOWNLOADS_WEB_URL_TEMPLATE.format(**context),
+            item_copyright=info["license"],
+            pubdate=_datetime_from_iso_date(info.get("releasedate")),
+            updateddate=_datetime_from_iso_date(info.get("releasedate")),
+        )
+    return str(feed.writeString(encoding="utf-8"))
 
 
 def _datetime_from_iso_date(iso_date: str | None) -> datetime | None:
--- tests/test_feed.py
+++ tests/test_feed.py
@@ -38,23 +38,6 @@ def fixture_now() -> Callable[[], datetime]:
 
 
 def test_from_dict(neo_package: dict[str, Any], now: Callable[..., datetime]) -> None:
-    expected = """<?xml version='1.0' encoding='UTF-8'?>
-<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="de">
-  <id>https://mediola.com/service/downloads</id>
-  <title>neo – Releases</title>
-  <updated>2022-10-01T14:46:53+02:00</updated>
-  <link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo" rel="alternate" type="text/html"/>
-  <generator uri="https://lkiesow.github.io/python-feedgen" version="1.0.0">python-feedgen</generator>
-  <subtitle>Software releases for neo</subtitle>
-  <entry>
-    <id>https://mediola.com/service/downloads/versions/2.11.3</id>
-    <title>2.11.3</title>
-    <updated>2022-09-26T00:00:00+02:00</updated>
-    <content>Version 2.11.3</content>
-    <link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo"/>
-    <published>2022-09-26T00:00:00+02:00</published>
-    <rights>https://www.mediola.com/eula</rights>
-  </entry>
-</feed>
-"""  # pylint: disable=line-too-long
-    assert feed.from_dict("neo", neo_package, now) == expected
+    expected = """<?xml version="1.0" encoding="utf-8"?>
+<feed xml:lang="de" xmlns="http://www.w3.org/2005/Atom"><title>neo – Releases</title><link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo" rel="alternate"></link><id>https://mediola.com/service/downloads</id><updated>2022-09-26T00:00:00+02:00</updated><subtitle>Software releases for neo</subtitle><entry><title>2.11.3</title><link href="https://www.mediola.com/service#downloads?type=software&amp;product=neo" rel="alternate"></link><published>2022-09-26T00:00:00+02:00</published><updated>2022-09-26T00:00:00+02:00</updated><id>https://mediola.com/service/downloads/versions/2.11.3</id><summary type="html">Version 2.11.3</summary><rights>https://www.mediola.com/eula</rights></entry></feed>"""  # pylint: disable=line-too-long
+    assert feed.from_dict("neo", neo_package, now).strip() == expected
